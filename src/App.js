b307b3755f66f8b0d8bf1d9df6f23f830a5746ff
Hanuman Singh <hanuman22698@gmail.com>
2022-06-05 11:51:28 +0530

    added localstorage filters data

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Table from './DynamicTable/Table';
 import './api'
 import { getAPIData } from './api';
 import { Pagination } from './components/Pagination';
-import { sortAsec, sortDesc, validateData } from './utils';
+import { getLocalStorage, saveToLocalStorage, sortAsec, sortDesc, validateData } from './utils';
 
 const theadData = ["Name", "Price", "Percent"];
 
+const localStorageData = getLocalStorage('tableData')
+
+const defaultValues = {
+  keyword: localStorageData?.keyword ? localStorageData.keyword : '',
+  bodyData: localStorageData?.bodyData ? localStorageData.bodyData : [],
+  pageNum: localStorageData?.pageNum ? localStorageData.pageNum : 1,
+  isAscending: true,
+  tableDataRef: localStorageData?.refData ? localStorageData.refData : []
+}
+
 function App() {
-  const [keyword, setKeyword] = useState('')
-  const [bodyData, setBodyData] = useState([])
-  const [pageNum, setPageNum] = useState(1);
-  const [isAscending, setIsAscending] = useState(false)
-  const tableDataRef = useRef([]);
-  const [notFound, setNotFound] = useState(false)
+  const [keyword, setKeyword] = useState(defaultValues.keyword);
+  const [bodyData, setBodyData] = useState(defaultValues.bodyData);
+  const [pageNum, setPageNum] = useState(defaultValues.pageNum);
+  const [isAscending, setIsAscending] = useState(defaultValues.isAscending);
+  const tableDataRef = useRef(defaultValues.tableDataRef);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     let temp = tableDataRef.current?.filter(({ items }) => validateData(items.join('')).indexOf(validateData(keyword)) > -1);
@@ -24,23 +34,27 @@ function App() {
     else {
       setNotFound(false)
     }
+    saveToLocalStorage('tableData', { keyword })
     setBodyData(temp)
   }, [keyword])
 
   const getData = async () => {
     const tdata = await getAPIData(pageNum)
-    let sortedData = isAscending ? sortDesc(tdata) : sortAsec(tdata)
+    let sortedData = isAscending ? sortAsec(tdata) : sortDesc(tdata)
     tableDataRef.current = sortedData;
     setBodyData(sortedData)
+    return new Promise((res, reject) => res(sortedData))
   }
 
   useEffect(() => {
-    getData()
+    if (pageNum !== localStorageData?.pageNum)
+      getData().then(() => saveToLocalStorage('tableData', { pageNum, bodyData, refData: tableDataRef.current }))
   }, [pageNum])
+
 
   const handleSorting = () => {
     setIsAscending(prev => !prev)
-    let sortedData = isAscending ? sortAsec(bodyData) : sortDesc(bodyData)
+    let sortedData = isAscending ? sortDesc(bodyData) : sortAsec(bodyData)
     setBodyData(sortedData)
   }
 
@@ -65,7 +79,7 @@ function App() {
         <input type="search" className='search-box' value={keyword} onChange={e => setKeyword(e.target.value)} placeholder="Search..." title="Type in a name" />
         <Pagination pageNum={pageNum} handleNext={handleNext} handlePrev={handlePrev} />
       </div>
-      {tableDataRef.current.length === 0 ? <p className='title'>Loading...</p> : <Table theadData={theadData} tbodyData={bodyData} handleSorting={handleSorting} isAscending={isAscending} />}
+      {tableDataRef.current?.length === 0 ? <p className='title'>Loading...</p> : <Table theadData={theadData} tbodyData={bodyData} handleSorting={handleSorting} isAscending={isAscending} />}
       {notFound && <p className='error'> {keyword} is not found, please try again with some other keyword !!</p>}
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,17 @@ export const validateData = (data) => data.trim().toLowerCase();
 // Removes period, comma, spaces from data
 const regex = /[.,\s]/g;
 export const sanitizeData = (data) => data.replace(regex, '')
+
+export const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key))
+
+export const saveToLocalStorage = (key, data) => {
+    let localStorageData = getLocalStorage(key)
+    if (!localStorageData) {
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+    else {
+        localStorage.setItem(key, JSON.stringify({ ...localStorageData, ...data }))
+    }
+}
+
+
